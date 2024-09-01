79a59dde50cdd64730c25c756b3d61bbefa11b1d
EmptinessOfNature <lipengyubz@163.com>
2024-09-02 00:28:01 +0800

    改写策略ing

--- macd.py
+++ macd.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from macd_utils import tdx_raw2_kline, double_macd, jw, duanxian
 
 
@@ -49,6 +51,7 @@ def is_chuan(data, col, pre_len, post_len, thresh, direction):
     return ret
 
 def is_v(data,col,pre_len,post_len,direction):
+    # is_v计算周期至少是三个点，所以如果输入pre和post都是1，那么也会计算最近三个点。
     def _is_v_bottom(row,col,pre_len,post_len,direction):
         index = row.name
         if index < pre_len + post_len:
@@ -57,7 +60,12 @@ def is_v(data,col,pre_len,post_len,direction):
                      index - pre_len - post_len : index - post_len+1
                      ]
         values_post = data[col].iloc[index - post_len: index + 1]
-        ret = bool(((values_pre.diff()[-pre_len:]<0).all()) & ((values_post.diff()[-post_len:]>0).all()))
+        if direction=='bottom':
+            ret = bool(((values_pre.diff()[-pre_len:]<0).all()) & ((values_post.diff()[-post_len:]>0).all()))
+        elif direction=='top':
+            ret = bool(((values_pre.diff()[-pre_len:]>0).all()) & ((values_post.diff()[-post_len:]<0).all()))
+        else:
+            raise ValueError("is_v函数的direction错误")
         return ret
 
     ret = data.apply(_is_v_bottom,axis=1,col=col,pre_len=pre_len,post_len=post_len,direction='bottom')
@@ -68,23 +76,15 @@ def speed(data, col, length):
     ret = data[col].diff(periods=length) / length
     return ret
 
+def condition_or(rules):
+    # 多个条件取或
+    merged = pd.concat(rules,axis=1,ignore_index=True)
+    ret = merged.any(axis=1)
+    return ret
+
 
 def merge_signal(data):
-    # long_rules.append(
-    #     data.loc[i, "m1"] > 0
-    #     and abs(data.loc[i, "m1"]) > 1
-    #     and is_chuan(data, i, "m2", 1, 1, "up", 0)
-    #     and speed_abs(data, i, "m2", 2) > 0.1
-    # )
-    # long_rules.append(
-    #     data.loc[i, "m1"] > 0
-    #     and abs(data.loc[i, "m1"]) > 1
-    #     and data.loc[i, "m2"] < 0
-    #     and is_v(data, i, "m2", 3, 3, "bottom")
-    #     and abs(data.loc[i, "m1"]) >= 2 * abs(data.loc[i, "m2"])
-    # )
-
-    data['is_v'] = is_v(data,col='close',pre_len=2,post_len=2,direction='bottom')
+    # 开多仓策略
     long_start_rules = []
     long_start_rules.append(
         (data["m1"] > 0)
@@ -96,14 +96,43 @@ def merge_signal(data):
         (data["m1"] > 0)
         & (data["m1"].abs() > 1)
         & (data['m2']<0)
-        & (speed(data, col="m2", length=2) > 0.1)
+        & (is_v(data,col='m2',pre_len=3,post_len=3,direction='bottom'))
+        & (data['m1'].abs()>=2*data['m2'].abs())
     )
+    long_start = condition_or(long_start_rules)
 
-    # data['is_chuan_up']  = is_chuan(data,col='m1',pre_len=2,post_len=2,thresh=0,direction='up')
-    # data['is_chuan_down'] = is_chuan(data,col='m1',pre_len=2,post_len=2,thresh=0,direction='down')
-    # data['m2_speed'] = speed(data,col='m2',length=2)
-    # 规则1
-    (data["m1"] > 0) & (data["m1"].abs() > 1)
+    # 平多仓策略
+    long_end_rules = []
+    long_end_rules.append((is_chuan(data,col='m2',pre_len=1,post_len=1,thresh=0,direction='down')))
+    long_end_rules.append((is_v(data,col='m2',pre_len=3,post_len=3,direction='top')))
+    long_end_rules.append(speed(data,col='m2',length=2).abs()<0.01)
+    long_end = condition_or(long_end_rules)
+
+    # # 做空开单条件
+    #
+    # short_rules.append(
+    #     data.loc[i, "m1"] < 0
+    #     and abs(data.loc[i, "m1"]) > 1
+    #     and is_chuan(data, i, "m2", 1, 1, "down", 0)
+    #     and speed_abs(data, i, "m2", 2) > 0.1
+    # )
+    # short_rules.append(
+    #     data.loc[i, "m1"] < 0
+    #     and abs(data.loc[i, "m1"]) > 1
+    #     and data.loc[i, "m2"] > 0
+    #     and is_v(data, i, "m2", 3, 3, "top")
+    #     and abs(data.loc[i, 'm2']) > 0.5
+    #     and abs(data.loc[i, "m1"]) >= 2 * abs(data.loc[i, "m2"])
+    # )
+    # # 做空止盈止损条件
+    #
+    # short_stop_rules.append(is_chuan(data, i, "m2", 1, 1, "up", 0))
+    # short_stop_rules.append(is_v(data, i, "m2", 3, 3, "bottom"))
+    # short_stop_rules.append(speed_abs(data, i, "m2", 2) < 0.01)
+    #
+    # 空仓开仓策略
+
+
     return data
 
 
